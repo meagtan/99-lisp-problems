4dfef6d427af1382a936fb68509119c067e41419
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-13 10:23:30 +0300

    Finished p22-p28

--- lists/p22-p28.lisp
+++ lists/p22-p28.lisp
@@ -1,6 +1,7 @@
 ;;;; Further index operations
 
 ;;; p22
+
 (defun range (i k)
   "Create a list containing all integers within a given range.
 If first argument is smaller than second, produce a list in decreasing order.
@@ -14,6 +15,7 @@ Example:
          (cons i (range (1+ i) k)))))
          
 ;;; p23
+
 (defun rnd-select (list k)
   "Extract a given number of randomly selected elements from a list.
 The selected items shall be returned in a list.
@@ -27,6 +29,7 @@ Example:
                         (1- k))))))
                         
 ;;; p24
+
 (defun lotto-select (n m)
   "Lotto: Draw N different random numbers from the set 1..M.
 The selected numbers shall be returned in a list.
@@ -37,6 +40,7 @@ Example:
               n)) ;p23
 
 ;;; p25
+
 (defun rnd-permu (list)
   "Generate a random permutation of the elements of a list.
 Example:
@@ -45,6 +49,7 @@ Example:
   (rnd-select list (length list))) ;p23
   
 ;;; p26
+
 (defun combination (k list)
   "Generate the combinations of K distinct objects chosen from the N elements of a list
 In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients). For pure mathematicians, this result may be great. But we want to really generate all the possibilities in a list.
@@ -60,6 +65,7 @@ Example:
 
 
 ;;; p27
+
 (defun group3 (list)
   "Group the elements of a set into disjoint subsets.
 a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? 
@@ -92,4 +98,66 @@ Example:
 
 (defun permutations (list)
   "Generate all the different permutations of LIST, accounting for reoccurring terms."
-  (
+  (if list
+      (mapcar (lambda (item)
+                (mapcar (lambda (perm) (cons item perm))
+                        (permutations (remove-item item list))))
+              (remove-duplicates list))
+      (list NIL)))
+      
+(defun remove-item (item list)
+  "Remove all instances of ITEM from LIST."
+  (when list
+    (if (eq item (car list))
+        (remove-item (cdr list))
+        (cons (car list)
+              (remove-item (cdr list))))))
+
+(defun remove-duplicates (list)
+  "Remove all duplicate elements from LIST."
+  (when list
+    (cons (car list)
+          (remove-duplicates 
+            (remove-item (car list) (cdr list))))))
+
+;;; p28
+
+(defun lsort (list)
+  "Sorting a list of lists according to length of sublists
+a) We suppose that a list contains elements that are lists themselves. 
+The objective is to sort the elements of this list according to their length. 
+E.g. short lists first, longer lists later, or vice versa.
+
+Example:
+* (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+((O) (D E) (D E) (M N) (A B C) (F G H) (I J K L))"
+  (sort list (lambda (s1 s2) (< (length s1) (length s2)))))
+      
+(defun lfsort (list)
+  "b) Again, we suppose that a list contains elements that are lists themselves. 
+  But this time the objective is to sort the elements of this list according to their length frequency;
+  i.e., in the default, where sorting is done ascendingly, lists with rare lengths are placed first, 
+  others with a more frequent length come later.
+
+Example:
+* (lfsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+((i j k l) (o) (a b c) (f g h) (d e) (d e) (m n))"
+  (let ((freqs (mapcar #'reverse (encode (mapcar #'length list))))) ;p10
+    (sort list (lambda (s1 s2) (< (assoc (length s1) freqs) 
+                                  (assoc (length s2) freqs))))))
+
+(defun sort (list pred)
+  "Sort list according to the ordering defined by PRED, assumed analogous to <."
+  (when list
+    (let ((partition (partition (cdr list)
+                                (lambda (item) (funcall pred item (car list))))))
+      (append (sort (car partition) pred) (cons (car list) (sort (cdr partition) pred))))))
+
+(defun partition (list pred)
+  "Separate list into two sublists, the first one containing elements that verify PRED, the second one containing the rest."
+  (if list
+      (let ((res (partition (cdr list) pred)))
+        (if (funcall pred (car list))
+            (cons (cons (car list) (car res)) (cdr res))
+            (list (car res) (cons (car list) (cadr res)))))
+      (list NIL NIL)))
