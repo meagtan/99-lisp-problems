f5d42456cf34a67b0ce5bfb07b31a9fccf189b61
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-13 16:17:49 +0300

    Update p64-p66.lisp

--- bintrees/p64-p66.lisp
+++ bintrees/p64-p66.lisp
@@ -1,1 +1,36 @@
 ;;;; Binary tree layout
+
+;;; A layout assigns a pair of coordinates to each node,  so the node (item left right) is transformed into 
+;;;   ((item (x . y)) left right).
+
+;;; p64
+
+(defun layout1 (tree &optional (level 1) (left-cousins 0))
+  "Lay out binary tree so that the x-coordinate of each node represents its position on the inorder sequence 
+and the y-coordinate its level."
+  (and tree (tree-p tree)
+    (list (list (first tree) (cons (+ 1 left-cousins (length (nodes (second tree)))) level))
+          (layout1 (second tree) (1+ level) left-cousins)
+          (layout1 (third tree) (1+ level) (+ 1 left-cousins (length (nodes (second tree))))))))
+
+;;; p65
+
+(defun layout2 (tree &optional (level 1) (distance (expt 2 (- (height tree) 2))) (start-x (start-x tree)))
+  "Lay out binary tree so that the horizontal distance between each neighbor at a given level is constant."
+  (and tree (tree-p tree)
+    (list (list (first tree) (cons start-x level))
+          (layout2 (second tree) (1+ level) (/ distance 2) (- start-x distance))
+          (layout2 (third  tree) (1+ level) (/ distance 2) (+ start-x distance)))))
+
+(defun start-x (tree &aux (distance (expt 2 (- (height tree) 2))))
+  "The x-coordinate of the root node of tree in layout2."
+  (if (second tree)
+      (+ distance (start-x (second tree)))
+      0))
+
+;;; p66
+
+(defun layout3 (tree &optional (level 1))
+  "Lay out binary tree so that the horizontal distance between each neighbor at a given level is constant and as small as
+possible without two nodes occupying the same position."
+  )
