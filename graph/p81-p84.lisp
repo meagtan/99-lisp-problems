e6e2e5d409898597789b4b17844d0e4e10ff35c4
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-14 12:02:16 +0300

    Update p81-p84.lisp

--- graph/p81-p84.lisp
+++ graph/p81-p84.lisp
@@ -21,20 +21,29 @@
 
 ;;; p83
 
-(defun s-trees (graph)
-  "Construct all spanning trees of a graph via backtracking."
-  )
+(defun s-trees (graph &optional visited-ns visited-es)
+  "Generate all spanning trees of GRAPH, where trees are represented as lists of edges."
+  (munion (lambda (node)
+             (munion (lambda (edge) 
+                        (unless (subsetp (nodes edge) visited-ns)
+                          (munion (lambda (tree) (cons edge tree))
+                            (s-trees graph (cons node visited-ns)
+                                           (cons edge visited-es)))))
+               (set-difference (edges graph node) visited-es)))
+           (graph-nodes graph)))
 
 (defun s-tree-p (graph tree)
   "Return T if TREE is a spanning tree of GRAPH."
-  (and (subgraph-p tree graph)
-       (tree-p tree) ;will this halt?
-       (null (set-exclusive-or (graph-nodes tree) (graph-nodes graph)))))
+  (member tree (s-trees graph)))
 
 (defun tree-p (graph)
   "Return T if GRAPH is a tree."
-  (member graph (s-trees graph)))
+  (s-tree-p graph graph))
 
 (defun connected-p (graph)
   "Return T if GRAPH is connected."
   (not (null (s-trees graph))))
+
+(defun munion (function list &rest lists)
+  "Return the union of the application of FUNCTION to successive elements of LIST."
+  (reduce #'union (apply #'mapcar function list lists)))
