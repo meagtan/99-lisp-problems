4978452d093d4935a5b6a199196ab829b48a72f3
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-14 10:22:14 +0300

    Finished graph representations

--- graph/p80.lisp
+++ graph/p80.lisp
@@ -1,1 +1,108 @@
 ;;;; Graph formats, conversions
+
+;;; p80
+
+;;; We shall consider three formats each for graphs, digraphs and weighted graphs:
+;;; - (default) Graph-term form, containing sets of nodes and edges.
+;;; - Adjacency list form, an association list mapping each node to its neighboring nodes.
+;;; - Human readable form, containing either edges or sole nodes for nodes not connected to any other node.
+
+;;; An edge is either a cons pair of nodes, in the case of unweighted edges, or a list of nodes and a weight.
+
+(defstruct graph 
+  "A graph containing nodes and edges, which might be weighted or unweighted."
+  nodes edges directed)
+
+(defun make-ud-graph (nodes edges)
+  "Make undirected graph with given nodes and edges."
+  (make-graph :nodes nodes :edges edges :directed NIL))
+  
+(defun make-d-graph (nodes edges)
+  "Make directed graph with given nodes and edges."
+  (make-graph :nodes nodes :edges edges :directed T))
+
+(defun adjacency-list (graph)
+  "Return the adjacency list of a graph."
+  (mapcar (lambda (node)
+            (cons node 
+                  (mapcar #'cdr 
+                    (remove-if-not (lambda (edge) (edge-of edge node graph)) 
+                                   (graph-edges graph)))))
+          (graph-nodes graph)))
+
+(defun readable-list (graph)
+  "Return a list of edges and lone nodes of a graph."
+  (append (graph-edges graph)
+          (set-difference (graph-nodes graph)
+            (mappend #'nodes (graph-edges graph)))))
+
+(defun ud-adjacency-graph (adj-list &aux (nodes (mapcar #'car adj-list)))
+  "Make undirected graph from the given adjacency list."
+  (make-ud-graph
+    nodes
+    (let (edges)
+      (mapc (lambda (n)
+        (mapc (lambda (x &aux (e (cons n x)))
+          (unless (member e edges :test (lambda (l1 l2) (null (set-exclusive-or l1 l2)))
+                                  :key #'nodes)
+            (push e edges)))
+          (cdr (assoc n adj-list))))
+        nodes)
+      edges)))
+
+(defun d-adjacency-graph (adj-list &aux (nodes (mapcar #'car adj-list)))
+  "Make directed graph from the given adjacency list."
+  (make-d-graph
+    nodes
+    (let (edges)
+      (mapc (lambda (n)
+        (mapc (lambda (x &aux (e (cons n x)))
+                (push e edges))
+          (cdr (assoc n adj-list))))
+        nodes)
+      edges)))
+
+(defun ud-readable-graph (list &aux (lone-nodes (remove-if-not #'atom list)) (edges (remove-if-not #'edge-p list)))
+  "Make undirected graph from the given readable list of edges and lone nodes."
+  (make-ud-graph (append lone-nodes
+                         (mappend #'nodes edges))
+                 edges))
+
+(defun d-readable-graph (list &aux (lone-nodes (remove-if-not #'atom list)) (edges (remove-if-not #'edge-p list)))
+  "Make directed graph from the given readable list of edges and lone nodes."
+  (make-d-graph (append lone-nodes
+                        (mappend #'nodes edges))
+                edges))
+
+(defun edge-of (edge node graph)
+  "Return T if edge starts from node."
+  (or (eq node (car edge))
+      (if (not (graph-directed graph)) ;if undirected, check end
+          (eq node (cadr (nodes edge))))))
+
+(defun nodes (edge)
+  "Return the nodes of an edge."
+  (cons (car edge)
+        (cons (if (consp (cdr edge))
+                  (cadr edge)
+                  (cdr edge))
+              NIL)))
+
+(defun edge-p (edge)
+  "Return T if the given expression is a valid edge."
+  (and (consp edge)
+       (or (atom (cdr edge))
+           (atom (cadr edge))
+           (null (cdddr edge)))))
+
+(defun edge (graph node1 node2)
+  "If there is an edge from NODE1 to NODE2, return T (or the weight of the edge if the graph is weighted)."
+  (let ((edges (assoc node1 (adjacency-list graph))))
+    (if edges
+      (let ((edge-end (find node2 (cdr edges) 
+                        :test (lambda (n e)
+                                (or (eq n e)
+                                    (if (consp e)
+                                      (eq n (car e))))))))
+        (or (atom edge-end)
+            (cadr edge-end))))))
