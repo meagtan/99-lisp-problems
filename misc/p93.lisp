382283b2fd6c23c1665ceeff4137644b2c0364d8
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-17 13:31:34 +0300

    Update p93.lisp

--- misc/p93.lisp
+++ misc/p93.lisp
@@ -1,1 +1,33 @@
 ;;;; An arithmetic puzzle
+
+(defun find-equations (numbers)
+  "Given a list of integer numbers, find ways of inserting operators such that the result is a correct equation. 
+Example: With the list of numbers [2,3,5,7,11] we can form the equations 2-3+5+7 = 11 or 2 = (3*5+7)/11 (and ten others!)."
+  ;; break down position of = sign
+  (when numbers
+    (mapcan (lambda (i &aux (sides (split numbers i))) ;p17
+              (make-equations (car sides) (cadr sides)))
+      (range 1 (1- (length numbers)))))) ;p22
+
+(defun make-equations (lhs rhs)
+  "Return all equations whose left-hand side is formed from LHS and right-hand side from RHS by inserting operators."
+  (if (< (length lhs) (length rhs))
+      (mapcan (lambda (lhs-expr)
+                (mapcar (lambda (rhs-expr)
+                          (append lhs-expr (list '=) rhs-expr))
+                        (make-exprs-value rhs (value-of lhs-expr))))
+              (make-exprs lhs))
+      (mapcan (lambda (rhs-expr)
+                (mapcar (lambda (lhs-expr)
+                          (append lhs-expr (list '=) rhs-expr))
+                        (make-exprs-value lhs (value-of rhs-expr))))
+              (make-exprs rhs))))
+
+(defun make-exprs (numbers)
+  "Generate all expressions formed by inserting operators to NUMBERS."
+  (if (= (length numbers) 1)
+      numbers
+      ;; try out each operator, insert operator after (car numbers) and, for operators other than +, also start parenthesis
+      ;; close parenthesis before * or /
+      ))
+      
