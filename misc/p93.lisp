22ebb9001aa64149796a014d0335922753705963
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-17 22:58:23 +0300

    Update p93.lisp

--- misc/p93.lisp
+++ misc/p93.lisp
@@ -23,7 +23,7 @@ Example: With the list (2 3 5 7 11) we can form the equations (2 - 3 + 5 + 7 = 1
                         ;; remove all but the expressions whose value is the same as lhs-expr
                         (remove lhs-expr (make-exprs rhs)
                           :test-not #'eql
-                          :key #'eval))
+                          :key #'eval)))
               (make-exprs lhs))
       (mapcan (lambda (rhs-expr)
                 (mapcar (lambda (lhs-expr)
@@ -31,7 +31,7 @@ Example: With the list (2 3 5 7 11) we can form the equations (2 - 3 + 5 + 7 = 1
                         ;; remove all but the expressions whose value is the same as rhs-expr
                         (remove rhs-expr (make-exprs lhs)
                           :test-not #'eql
-                          :key #'eval))))
+                          :key #'eval)))
               (make-exprs rhs))))
 
 (defun make-exprs (numbers)
@@ -42,7 +42,7 @@ Example: With the list (2 3 5 7 11) we can form the equations (2 - 3 + 5 + 7 = 1
       (loop for i from 1 to (1- (length numbers))
             for partition = (split numbers i) append 
           (loop for arg1 in (make-exprs (car partition)) append 
-              (loop for arg2 in (make-exprs (cdr partition)) collect 
+              (loop for arg2 in (make-exprs (cadr partition)) append 
                 (apply-binary-ops arg1 arg2))))))
 
 (defun apply-unary-ops (arg1)
@@ -58,13 +58,13 @@ Example: With the list (2 3 5 7 11) we can form the equations (2 - 3 + 5 + 7 = 1
 (defun apply-op (op arg1 arg2)
   "Apply operator to arguments, disregarding associativity."
   (unless (and (eq op '/) (zerop (eval arg2)))
-    (list arg1 arg2)))
+    (list op arg1 arg2)))
 
 ;;; An aside: converting from lispy arithmetic expressions to infix notation
 ;;; Not necessary for the purpose of the program, as the order of numbers stays the same, but truer to the original problem.
 
 (defparameter *unary-ops*  '(-) "Unary operators.")
-(defparameter *binary-ops* '(+ - * / =) "Binary operators, sorted by precedence.")
+(defparameter *binary-ops* '(= + - * /) "Binary operators, sorted by precedence.")
 
 (defun prefix-to-infix (expr)
   "Convert a Lisp arithmetic expression in Polish notation into a list of numbers and operators in infix notation.
@@ -72,18 +72,32 @@ Example: (+ (* 3 5) (- 2 7)) => (3 * 5 + 2 - 7); (* (+ 3 5) (- 2 7)) => ([ 3 + 5
   (cond ((numberp expr) (list expr))
         ((unary-p expr) expr)
         ((binary-p expr)
-         (append (infix-arg (first expr) (second expr))
+         (append (infix-arg (first expr) (second expr) T)
                  (cons (first expr)
-                   (infix-arg (first expr) (third expr))))))
+                   (infix-arg (first expr) (third expr) NIL))))))
 
-(defun infix-arg (op expr &aux (res (prefix-to-infix expr)))
+(defun infix-arg (op expr lhs &aux (res (prefix-to-infix expr)))
   "Optionally add parentheses to the infix notation of EXPR, as an argument to OP."
   (if (or (not (binary-p expr))
-          ;; compare precedence
-          (< (position op *binary-ops*)
-             (position (first expr) *binary-ops*)))
+          (precedes-p op (first expr) lhs))
       res
       (cons '[ (append res (list '])))))
+
+(defun precedes-p (op1 op2 lhs)
+  "Return T if an expression of OP2 can be written without parentheses at the LHS/RHS of an expression of OP1."
+  )
+
+(defun unary-p (expr)
+  "Return T if EXPR is the application of a unary operator."
+  (and (listp expr)
+       (= (length expr) 2)
+       (member (first expr) *unary-ops*)))
+
+(defun binary-p (expr)
+  "Return T if EXPR is the application of a binary operator."
+  (and (listp expr)
+       (= (length expr) 3)
+       (member (first expr) *binary-ops*)))
 
 (defun infix-to-prefix (expr)
   "Convert an infix arithmetic expression, expressed as a list of numbers, operators and brackets, into a Lisp expression."
