24bcf8ab907c24d241a0679100dfa6a5740fb656
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-07-24 08:24:45 +0300

    Update p99.lisp

--- misc/p99.lisp
+++ misc/p99.lisp
@@ -4,6 +4,13 @@
 
 (defun solve-puzzle (word-list puzzle)
   "Solve a crossword puzzle, inputted as a string, and return the solved puzzle as a string."
+  (let ((graph (assign-words word-list (puzzle-graph puzzle))))
+    (when graph
+      (graph-puzzle graph))))
+
+(defun assign-words (word-list graph)
+  "Assign words in the given list to each site of GRAPH, in a way compatible with the intersections of each site."
+  ;; go through each site, assign words of appropriate length, filter based on intersections
   )
 
 ;; The framework of a crossword puzzle is represented internally as a graph, with nodes representing sites, storing 
@@ -20,6 +27,7 @@
 
 (defun puzzle-graph (puzzle)
   "Convert the string representation of a puzzle into a graph."
+  ;; scan row by row
   )
 
 (defun graph-puzzle (graph)
