82bd35b1c161aaba37306332505700c26721296c
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-09-05 11:52:23 +0300

    Fixed algorithms, converted recursion into iteration

--- src/80-89 graphs/p81-p84.lisp
+++ src/80-89 graphs/p81-p84.lisp
@@ -4,14 +4,17 @@
 
 ;;; p81
 
-(defun paths (graph a b &optional visited)
+(defun paths (graph a b)
   "Return all acyclic paths from A to B."
-  (mapcan (lambda (edge &aux (n (end-node edge)))
-            (unless (member n visited)
-              (if (eq n b)
-                  (list edge)
-                  (cons edge (paths graph n b (cons a visited))))))
-          (edges a graph)))
+  (do ((stack (list (list a)) (cdr stack)) ;each element of the stack is an incomplete path in reverse order
+       res)
+      ((null stack) res)
+      (dolist (edge (edges (caar stack) graph))
+        (let ((n (other-node edge (caar stack))))
+          (unless (member n (cdar stack))
+            (if (equal n b) ;end of path
+                (push (reverse (car stack)) res)
+                (push (cons n (car stack)) (cdr stack))))))))
 
 ;;; p82
 
@@ -24,16 +27,19 @@
 ;;; p83
 
 ;; can this be simplified? what can one say about the different trees calculated?
-(defun s-trees (graph &optional visited-ns visited-es)
+(defun s-trees (graph &aux (len (length (graph-nodes graph))))
   "Generate all spanning trees of GRAPH, where trees are represented as lists of edges."
-  (munion (lambda (node)
-             (munion (lambda (edge) 
-                        (unless (subsetp (nodes edge) visited-ns)
-                          (mapcar (lambda (tree) (cons edge tree))
-                            (s-trees graph (cons node visited-ns)
-                                           (cons edge visited-es)))))
-               (set-difference (edges node graph) visited-es)))
-          (set-difference (graph-nodes graph) visited-ns)))
+  (do ((stack (list (cons NIL NIL))) ;each element of the stack contains list of visited edges and nodes
+       res)
+      ((null stack) res)
+      (let* ((elem (car stack)) 
+             (edges (car elem))
+             (nodes (cdr elem)))
+        (if (= (length nodes) len) ;complete tree
+            (pushnew edges res :test-not #'set-exclusive-or)
+            (dolist (edge (set-difference (graph-edges graph) edges)) ;perhaps include this in elem
+              (unless (subsetp (nodes edge) nodes) ;the inclusion of edge doesn't form a cycle
+                (push (cons (cons edge edges) (union (nodes edge) nodes)) (cdr stack))))))))
 
 (defun s-tree-p (graph tree)
   "Return T if TREE is a spanning tree of GRAPH."
@@ -47,27 +53,39 @@
   "Return T if GRAPH is connected."
   (not (null (s-trees graph))))
 
-(defun munion (function list &rest lists)
-  "Return the union of the application of FUNCTION to successive elements of LIST."
-  (reduce #'union (apply #'mapcar function list lists)))
-
 ;;; p84
 
-(defun ms-trees (graph &optional visited-ns visited-es)
-  "Return minimal spanning trees of GRAPH."
-  (munion (lambda (node 
-                   &aux (edges (remove-if (lambda (edge)
-                                            (subsetp (nodes edge) visited-ns))
-                                  (sort #'edge< (set-difference (edges node graph) visited-es)))))
-             ;; only pick the edge with the least weight
-             (when edges
-                (mapcar (lambda (tree) (cons (first edges) tree))
-                  (ms-trees graph (cons node visited-ns)
-                                  (cons (first edges) visited-es)))))
-          (set-difference (graph-nodes graph) visited-ns)))
+(defun ms-tree (graph)
+  "Return minimal spanning tree of GRAPH."
+  (do ((nodes (graph-nodes graph))
+       (costs (mapcar #'list (graph-nodes graph))) ;map nodes to least cost
+       (edges (mapcar #'list (graph-nodes graph))) ;map nodes to least costly edge
+       (res (make-graph))
+       node)
+      ((null nodes) res)
+      (setf node (pred-min nodes #'cost< :key (lambda (n) (assoc n costs)))) ;node with lowest cost
+      (setf nodes (remove node nodes :test #'equal))
+      (push node (graph-nodes res))
+      (when (cdr (assoc node edges))
+        (push (cdr (assoc node edges)) (graph-edges res)))
+      (dolist (edge (edges node graph))
+        (let ((other (other-node edge node)))
+          (and (member other nodes)
+               (cost< (edge-weight edge) (cdr (assoc other costs)))
+               (rplacd (assoc other costs) (edge-weight edge))
+               (rplacd (assoc other edges) edge))))))
 
-(defun edge< (edge1 edge2)
-  "Return T if EDGE1 has less weight, if any, than EDGE2."
-  (or (atom (cdr edge1))
-      (and (consp (cdr edge2))
-           (< (third edge1) (third edge2)))))
+(defun cost< (cost1 cost2)
+  "Order costs such that NIL corresponds to positive infinity."
+  (cond ((null cost1) NIL) ;also includes cost2 being NIL
+        ((null cost2) T)
+        (T (< cost1 cost2))))
+
+(defun pred-min (list pred &key (key identity))
+  "Return minimum of list based on predicate, such that (pred-min list #'<) is (apply #'min list)."
+  (do ((list (cdr list) (cdr list))
+       (min (car list)))
+      ((null list) min)
+      (if (funcall pred (funcall key (car list))
+                        (funcall key min))
+          (setf min (car list)))))
